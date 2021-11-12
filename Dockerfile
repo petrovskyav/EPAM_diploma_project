52e5dcacbff336d081a2ea4763f19783150eddf8
Alexander Petrovsky <Aleksandr_Petrovskii1@epam.com>
2021-11-12 10:03:19 +0300

    add env support

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 WORKDIR /usr/src/app
-COPY . /usr/src/app/
+COPY requirements.txt weather_collector/ /usr/src/app/
 RUN yum install -y python3 python3-pip
+RUN python3 -m venv venv
+RUN source venv/bin/activate
 RUN /usr/bin/pip3 install --upgrade pip
 RUN /usr/bin/pip3 install -r requirements.txt
 EXPOSE 80
--- weather_collector/weather_collector/settings.py
+++ weather_collector/weather_collector/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t)8$9v*ampc4h0$2r&ax_zr2b88emo6wm80d^%d_t7sgz5+f9+'
+#SECRET_KEY = 'django-insecure-t)8$9v*ampc4h0$2r&ax_zr2b88emo6wm80d^%d_t7sgz5+f9+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -76,12 +79,29 @@ WSGI_APPLICATION = 'weather_collector.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE", default='django.db.backends.mysql'),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
+
+
+
+
 
 
 # Password validation
